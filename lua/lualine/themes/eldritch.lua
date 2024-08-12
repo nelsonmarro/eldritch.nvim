70aaab737d31ba5450e45c3206febcee23674990
Nelson Marro <nelsonmarro99@gmail.com>
2024-08-12 16:35:10 -0500

    revert changes... sorry

--- lua/lualine/themes/eldritch.lua
+++ lua/lualine/themes/eldritch.lua
@@ -6,7 +6,7 @@ local eldritch = {}
 eldritch.normal = {
   a = { bg = colors.cyan, fg = colors.black },
   b = { bg = colors.fg_gutter, fg = colors.cyan },
-  c = { bg = colors.bg, fg = "#ABB4DA" },
+  c = { bg = colors.bg_highlight, fg = colors.fg_sidebar },
 }
 
 eldritch.insert = {
